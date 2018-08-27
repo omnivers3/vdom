d343a55de9d937f1337132687c7fa6c0ef027120
Perry Birch <perry_m_birch@homedepot.com>
2018-08-27 08:08:31 -0700

    nested

--- vdom/src/lib.rs
+++ vdom/src/lib.rs
@@ -4,18 +4,28 @@ pub trait IElement {
     type TAttributes;
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Element<TAttribute> {
     pub kind: String,
     pub attributes: Vec<TAttribute>,
 }
 
-#[derive(Debug)]
-pub struct Html<'a>(Element<&'a IHtmlAttribute>);
-
 pub trait IHtmlAttribute: std::fmt::Debug {}
 
 impl<'a> IHtmlAttribute for &'a IHtmlAttribute {}
+
+#[derive(Clone, Debug)]
+pub struct Html<T>(Element<T>) where T: IHtmlAttribute + Clone;
+// pub struct Html<'a>(Element<&'a IHtmlAttribute>);
+
+pub trait IBodyAttribute: std::fmt::Debug {}
+
+impl<'a> IBodyAttribute for &'a IBodyAttribute {}
+
+#[derive(Clone, Debug)]
+pub struct Body<T>(Element<T>) where T: IBodyAttribute + Clone;
+
+impl<T> IHtmlAttribute for Body<T> where T: IBodyAttribute + Clone {}
 
 // impl<'a, T> IElement for Html<'a>
 // where
@@ -30,15 +40,42 @@ pub struct Foo {}
 #[derive(Clone, Debug)]
 pub struct Bar {}
 
+#[derive(Clone, Debug)]
+pub struct Baz {}
+
 impl IHtmlAttribute for Foo {}
 
 impl IHtmlAttribute for Bar {}
 
-pub fn html<'a, 'b: 'a>(attributes: &'b [&IHtmlAttribute]) -> Html<'b>
+impl IBodyAttribute for Bar {}
+
+impl IBodyAttribute for Baz {}
+
+// pub fn html<'a, 'b: 'a, T: 'b>(attributes: &'b [&IHtmlAttribute]) -> Html<T>
+pub fn html<'a, 'b: 'a, T: 'b>(attributes: &'a [T]) -> Html<T>
+where
+    T: IHtmlAttribute + Clone,
+    &'b IHtmlAttribute: From<T>,
+    // &'b T: IHtmlAttribute + Clone,
+    // IHtmlAttribute: From<T>,
 {
     Html(
-        Element::<&IHtmlAttribute>{
+        Element::<T>{
             kind: "html".to_owned(),
+            attributes: attributes.to_vec(),
+            // attributes: attributes.to_vec().into_iter().map(|item| item.into()).collect(),
+        }
+    )
+}
+
+pub fn body<'a, T: 'a>(attributes: &'a [T]) -> Body<T>
+where
+    T: IBodyAttribute + Clone,
+    &'a IBodyAttribute: From<T>,
+{
+    Body(
+        Element::<T>{
+            kind: "body".to_owned(),
             attributes: attributes.to_vec(),
         }
     )
@@ -50,9 +87,18 @@ mod tests {
 
     #[test]
     fn should() {
-        let element = html(&[&Foo {}, &Bar {}]);
+        // let html1 = html(&[&Foo {}, &Bar {}, &Bar {}]);
+        let body1 = body(&[&Bar {}, &Baz {}]);
 
-        println!("{:?}", element);
+        let html2 = html(
+            &[ &body1
+            ]
+        );
+
+        // println!("{:?}", html1);
+        println!("{:?}", body1);
+
+        println!("{:?}", html2);
 
         assert!(false);
     }
