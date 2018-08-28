22bc298c7439bad2fdb3cf8a28cbe803913d84e5
Perry Birch <perry_m_birch@homedepot.com>
2018-08-28 12:46:51 -0700

    iter events

--- vdom/src/lib.rs
+++ vdom/src/lib.rs
@@ -1,105 +1,499 @@
+#[macro_use]
+extern crate serde_derive;
 
+use std::collections::hash_map::DefaultHasher;
+use std::fmt::{ Debug };
+use std::hash::{Hash, Hasher};
 
-pub trait IElement {
-    type TAttributes;
+/// Define the expected global trait set for events
+pub trait IEvent: Eq + Hash + PartialEq + Debug {}
+
+/// Auto-implement IEvent for types which share the required traits
+impl<TTarget> IEvent for TTarget
+where
+    TTarget: Eq + Hash + PartialEq + Debug,
+{}
+
+#[derive(Eq, Hash, PartialEq, Clone, Debug, Serialize, Deserialize)]
+pub enum EventTypes {
+    OnClick,
+}
+
+#[derive(Eq, Hash, PartialEq, Clone, Debug, Serialize, Deserialize)]
+pub struct Event<TEvents>
+where
+    TEvents: IEvent,
+{
+    pub event_type: EventTypes,
+    pub data: TEvents,
+}
+
+impl <TEvents> Event<TEvents>
+where
+    TEvents: IEvent,
+{
+    pub fn map<TWrappingEvents, FnWrapper>(
+        self,
+        wrapper: FnWrapper,
+    ) -> Event<TWrappingEvents>
+    where
+        TWrappingEvents: IEvent,
+        FnWrapper: FnOnce(TEvents) -> TWrappingEvents + Clone,
+    {
+        Event {
+            event_type: self.event_type,
+            data: wrapper(self.data),
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
-pub struct Element<TAttribute> {
+pub struct Node<TDatum> {
+    pub datum: TDatum,
+    pub children: Vec<Node<TDatum>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct CustomAttribute {
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct ClassAttribute {}
+
+#[derive(Clone, Debug)]
+pub struct EventAttribute<TEvents>
+where
+    TEvents: IEvent,
+{
+    event: TEvents,
+}
+
+#[derive(Clone, Debug)]
+pub enum AttributeTypes<TEvents>
+where
+    TEvents: IEvent,
+{
+    Custom (CustomAttribute),
+    Class (ClassAttribute),
+    Event (EventAttribute<TEvents>),
+}
+
+impl<TEvents> From<CustomAttribute> for AttributeTypes<TEvents>
+where
+    TEvents: IEvent,
+{
+    fn from(src: CustomAttribute) -> AttributeTypes<TEvents> {
+        AttributeTypes::Custom (src)
+    }
+}
+
+impl<TEvents> From<ClassAttribute> for AttributeTypes<TEvents>
+where
+    TEvents: IEvent,
+{
+    fn from(src: ClassAttribute) -> AttributeTypes<TEvents> {
+        AttributeTypes::Class (src)
+    }
+}
+
+impl<TEvents> From<EventAttribute<TEvents>> for AttributeTypes<TEvents>
+where
+    TEvents: IEvent,
+{
+    fn from(src: EventAttribute<TEvents>) -> AttributeTypes<TEvents> {
+        AttributeTypes::Event (src)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CustomElement {
     pub kind: String,
-    pub attributes: Vec<TAttribute>,
 }
 
-pub trait IHtmlAttribute: std::fmt::Debug {}
-
-impl<'a> IHtmlAttribute for &'a IHtmlAttribute {}
+#[derive(Clone, Debug)]
+pub struct HtmlElement {}
 
 #[derive(Clone, Debug)]
-pub struct Html<T>(Element<T>) where T: IHtmlAttribute + Clone;
-// pub struct Html<'a>(Element<&'a IHtmlAttribute>);
-
-pub trait IBodyAttribute: std::fmt::Debug {}
-
-impl<'a> IBodyAttribute for &'a IBodyAttribute {}
+pub struct BodyElement {}
 
 #[derive(Clone, Debug)]
-pub struct Body<T>(Element<T>) where T: IBodyAttribute + Clone;
+pub enum ElementTypes {
+    Custom (CustomElement),
+    Html (HtmlElement),
+    Body (BodyElement),
+}
 
-impl<T> IHtmlAttribute for Body<T> where T: IBodyAttribute + Clone {}
+impl From<CustomElement> for ElementTypes {
+    fn from(src: CustomElement) -> ElementTypes {
+        ElementTypes::Custom (src)
+    }
+}
 
-// impl<'a, T> IElement for Html<'a>
-// where
-//     T: IHtmlAttribute + Clone,
-// {
-//     type TAttributes = T;
+impl From<HtmlElement> for ElementTypes {
+    fn from(src: HtmlElement) -> ElementTypes {
+        ElementTypes::Html (src)
+    }
+}
+
+impl From<BodyElement> for ElementTypes {
+    fn from(src: BodyElement) -> ElementTypes {
+        ElementTypes::Body (src)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ElementDatum<TEvents>
+where
+    TEvents: IEvent,
+{
+    pub element: ElementTypes,
+    pub attributes: Vec<AttributeTypes<TEvents>>,
+}
+
+pub type Element<TEvents> = Node<ElementDatum<TEvents>>;
+
+impl<TEvents> Element<TEvents>
+where
+    TEvents: IEvent,
+{
+    pub fn new(datum: ElementDatum<TEvents>, children: Vec<Node<ElementDatum<TEvents>>>) -> Self {
+        Node::<ElementDatum<TEvents>> {
+            datum,
+            children,
+        }
+    }
+}
+
+pub fn element<TEvents>(kind: &str, attributes: &[AttributeTypes<TEvents>], children: &[Element<TEvents>]) -> Element<TEvents>
+where
+    TEvents: IEvent + Clone,
+{
+    let element = CustomElement { kind: kind.to_owned() }.into();
+    let attributes = attributes.to_vec();
+    let children = children.to_vec();
+    Element::<TEvents>::new(
+        ElementDatum::<TEvents> {
+            element,
+            attributes,
+        },
+        children,
+    )
+}
+// #[derive(Clone, Debug)]
+// pub struct Element<TAttributes> {
+//     pub kind: String,
+//     pub attributes: Vec<TAttributes>,
+//     pub children: Vec<Element<TAttributes>>,
 // }
 
-#[derive(Clone, Debug)]
-pub struct Foo {}
+// impl<TAttributes> Element<TAttributes>
+// where
+//     TAttributes: Clone + std::fmt::Debug,
+// {
+//     pub fn new(kind: &str, attributes: &[TAttributes], children: &[Element<TAttributes>]) -> Element<TAttributes> {
+//         Element {
+//             kind: kind.to_owned(),
+//             attributes: attributes.to_vec(),
+//             children: children.to_vec(),
+//         }
+//     }
+// }
 
-#[derive(Clone, Debug)]
-pub struct Bar {}
+// pub fn element<TAttributes, TChildren>(kind: String, attributes: &[TAttributes], children: &[TChildren]) -> ()
+// where
+//     TChildren: Into<Node<
+// {
 
-#[derive(Clone, Debug)]
-pub struct Baz {}
-
-impl IHtmlAttribute for Foo {}
-
-impl IHtmlAttribute for Bar {}
-
-impl IBodyAttribute for Bar {}
-
-impl IBodyAttribute for Baz {}
-
-// pub fn html<'a, 'b: 'a, T: 'b>(attributes: &'b [&IHtmlAttribute]) -> Html<T>
-pub fn html<'a, 'b: 'a, T: 'b>(attributes: &'a [T]) -> Html<T>
-where
-    T: IHtmlAttribute + Clone,
-    &'b IHtmlAttribute: From<T>,
-    // &'b T: IHtmlAttribute + Clone,
-    // IHtmlAttribute: From<T>,
-{
-    Html(
-        Element::<T>{
-            kind: "html".to_owned(),
-            attributes: attributes.to_vec(),
-            // attributes: attributes.to_vec().into_iter().map(|item| item.into()).collect(),
-        }
-    )
+// }
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
 
-pub fn body<'a, T: 'a>(attributes: &'a [T]) -> Body<T>
-where
-    T: IBodyAttribute + Clone,
-    &'a IBodyAttribute: From<T>,
-{
-    Body(
-        Element::<T>{
-            kind: "body".to_owned(),
-            attributes: attributes.to_vec(),
-        }
-    )
-}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn should() {
-        // let html1 = html(&[&Foo {}, &Bar {}, &Bar {}]);
-        let body1 = body(&[&Bar {}, &Baz {}]);
+    fn should_compose() {
+        let e: Element<()> = element("asdf",
+            &[ EventAttribute { event: () }.into()
+            ],
+            &[ element("blah", &[], &[])
 
-        let html2 = html(
-            &[ &body1
-            ]
-        );
+            ]);
 
-        // println!("{:?}", html1);
-        println!("{:?}", body1);
+        println!("{0:?}", e);
+        // let html1 = html(&[], &[]);
+        // let html2 = html(&[], &[html1.clone()]);
 
-        println!("{:?}", html2);
+        // println!("{0:?}", html1);
 
         assert!(false);
     }
+
+    // #[test]
+    // fn should_element() {
+    //     let element1 = Element::<()>::new("element-a", &[], &[]);
+    //     let element2 = Element::new("element-a", &[], &[element1.clone()]);
+    //     let element3 = Element::<()>::new("element-a", &[], &[Element::new("element-b", &[], &[])]);
+
+    //     println!("{0:?}", element1);
+    //     println!("{0:?}", element2);
+    //     println!("{0:?}", element3);
+
+    //     assert!(true);
+    // }
 }
+
+// pub trait IHtmlAttribute: std::fmt::Debug {}
+
+// impl<'a> IHtmlAttribute for &'a IHtmlAttribute {}
+
+// pub trait IHtmlChild: std::fmt::Debug {}
+
+// impl<'a> IHtmlChild for &'a IHtmlChild {}
+
+// #[derive(Clone, Debug)]
+// pub enum HtmlAttributes {
+// }
+
+// #[derive(Clone, Debug)]
+// pub enum HtmlChildren {
+//     Body (BodyChildren),
+// }
+
+// #[derive(Clone, Debug)]
+// pub enum BodyAttributes {
+
+// }
+
+// #[derive(Clone, Debug)]
+// pub enum BodyChildren {
+
+// }
+
+// impl From<BodyChildren> for HtmlChildren {
+//     fn from(source: BodyChildren) -> HtmlChildren {
+//         HtmlChildren::Body (source)
+//     }
+// }
+
+// #[derive(Clone, Debug)]
+// pub struct HtmlElement<TAttributes>(Element<TAttributes>);
+
+// pub fn html<TAttributes, TChildren>(
+//     attributes: &[TAttributes],
+//     children: &[TChildren]
+// ) -> HtmlElement<TAttributes>
+// where
+//     TAttributes: Into<HtmlAttributes> + Clone + std::fmt::Debug,
+//     TChildren: Clone + std::fmt::Debug,
+// {
+//     let children = children
+//         .into_iter()
+//         .map(|item| {
+//             let out: Element<TAttributes> = (*item).into();
+//             out
+//         })
+//         .collect()
+//         .to_vec();
+//     HtmlElement::<TAttributes>(
+//         Element::new("html", attributes, children)
+//     )
+// }
+
+// pub trait IElement {
+//     type TAttributes;
+// }
+
+// pub trait IAttribute: std::fmt::Debug {}
+
+// impl<'a> IAttribute for &'a IAttribute {}
+
+// pub trait IElement: std::fmt::Debug {}
+
+// impl<'a> IElement for &'a IElement {}
+
+// #[derive(Clone, Debug)]
+// pub struct Element<'a, TAttributes: 'a, TChildren: 'a>
+// where
+//     TAttributes: IAttribute + Clone,
+//     &'a IAttribute: From<TAttributes>,
+//     TChildren: IElement + Clone,
+//     &'a IElement: From<TChildren>,
+// {
+//     pub kind: &'a str,
+//     pub attributes: &'a [TAttributes],
+//     pub children: &'a [TChildren],
+// }
+
+// impl<'a, TAttributes: 'a, TChildren: 'a> IElement for Element<'a, TAttributes, TChildren>
+// where
+//     TAttributes: IAttribute + Clone,
+//     &'a IAttribute: From<TAttributes>,
+//     TChildren: IElement + Clone,
+//     &'a IElement: From<TChildren>,
+// {}
+
+// impl<'a, TAttributes: 'a, TChildren: 'a> Element<'a, TAttributes, TChildren>
+// where
+//     TAttributes: IAttribute + Clone,
+//     &'a IAttribute: From<TAttributes>,
+//     TChildren: IElement + Clone,
+//     &'a IElement: From<TChildren>,
+// {
+//     fn new(kind: &'a str, attributes: &'a [TAttributes], children: &'a [TChildren]) -> Element<'a, TAttributes, TChildren> {
+//         Element {
+//             kind,
+//             attributes,
+//             children,
+//         }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn should() {
+//         let element1 = Element::new("element-a", &[], &[]);
+//         let element2 = Element::new("element-a", &[], &[ &element1 ]);
+//         let element3 = Element::new("element-a", &[], &[ &Element::new("element-b", &[], &[])]);
+
+//         println!("{0:?}", element1);
+//         println!("{0:?}", element2);
+//         println!("{0:?}", element3);
+
+//         assert!(false);
+//     }
+// }
+
+// // let html1 = html(&[&Foo {}, &Bar {}, &Bar {}]);
+// let body1 = body(&[&Bar {}, &Baz {}]);
+
+// let html2 = html(
+//     &[ &Baz {}
+//     ],
+//     &[ &body1
+//     ]
+// );
+
+// // println!("{:?}", html1);
+// println!("{:?}", body1);
+
+// println!("{:?}", html2);
+
+// pub trait IHtmlAttribute: std::fmt::Debug {}
+
+// impl<'a> IHtmlAttribute for &'a IHtmlAttribute {}
+
+// pub trait IHtmlChild: std::fmt::Debug {}
+
+// impl<'a> IHtmlChild for &'a IHtmlChild {}
+
+// #[derive(Clone, Debug)]
+// pub struct Html<'a, TAttributes, TChildren>(Element<'a, TAttributes, TChildren>)
+// where
+//     TAttributes: IHtmlAttribute + Clone,
+//     &'a IHtmlAttribute: From<TAttributes>,
+//     TChildren: IHtmlChild + Clone,
+//     &'a IHtmlChild: From<TChildren>;
+
+// // pub struct Html<'a>(Element<&'a IHtmlAttribute>);
+
+// pub trait IBodyAttribute: std::fmt::Debug {}
+
+// impl<'a> IBodyAttribute for &'a IBodyAttribute {}
+
+// pub trait IBodyChild: std::fmt::Debug {}
+
+// impl<'a> IBodyChild for &'a IBodyChild {}
+
+// #[derive(Clone, Debug)]
+// pub struct Body<'a, TAttributes, TChildren>(Element<'a, TAttributes, TChildren>)
+// where
+//     TAttributes: IBodyAttribute + Clone,
+//     &'a IBodyAttribute: From<TAttributes>,
+//     TChildren: IBodyChild + Clone,
+//     &'a IBodyChild: From<TChildren>;
+
+// impl<'a, TAttributes, TChildren> IHtmlChild for Body<'a, TAttributes, TChildren>
+// where
+//     TAttributes: IBodyAttribute + Clone,
+//     TChildren: IBodyChild + Clone
+// {}
+
+// // impl<'a, T> IElement for Html<'a>
+// // where
+// //     T: IHtmlAttribute + Clone,
+// // {
+// //     type TAttributes = T;
+// // }
+
+// #[derive(Clone, Debug)]
+// pub struct Foo {}
+
+// #[derive(Clone, Debug)]
+// pub struct Bar {}
+
+// #[derive(Clone, Debug)]
+// pub struct Baz {}
+
+// impl IHtmlAttribute for Foo {}
+
+// impl IHtmlAttribute for Bar {}
+
+// impl IBodyAttribute for Bar {}
+
+// impl IBodyAttribute for Baz {}
+
+// // pub fn html<'a, 'b: 'a, T: 'b>(attributes: &'b [&IHtmlAttribute]) -> Html<T>
+// pub fn html<'a, TAttributes: 'a, TChildren: 'a>(
+//     attributes: &'a [TAttributes],
+//     children: &'a [TChildren]
+// ) -> Html<'a, TAttributes, TChildren>
+// where
+//     TAttributes: IHtmlAttribute + Clone,
+//     &'a IHtmlAttribute: From<TAttributes>,
+//     TChildren: IHtmlChild + Clone,
+//     &'a IHtmlChild: From<TChildren>,
+
+//     // &'b T: IHtmlAttribute + Clone,
+//     // IHtmlAttribute: From<T>,
+// {
+//     Html(
+//         Element::<TAttributes, TChildren>{
+//             kind: "html".to_owned(),
+//             attributes: attributes.to_vec(),
+//             children: children.to_vec(),
+//             // attributes: attributes.to_vec().into_iter().map(|item| item.into()).collect(),
+//         }
+//     )
+// }
+
+// pub fn body<'a, TAttributes: 'a, TChildren: 'a>(
+//     attributes: &'a [TAttributes],
+//     children: &'a [TChildren]
+// ) -> Body<'a, TAttributes, TChildren>
+// where
+//     TAttributes: IBodyAttribute + Clone,
+//     &'a IBodyAttribute: From<TAttributes>,
+//     TChildren: IBodyChild + Clone,
+//     &'a IBodyChild: From<TChildren>,
+// {
+//     Body(
+//         Element::<TAttributes, TChildren>{
+//             kind: "body".to_owned(),
+//             attributes: attributes.to_vec(),
+//             children: children.to_vec(),
+//         }
+//     )
+// }
+
